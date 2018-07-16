7cf5d640c82ed2e611ef74d4a8dd9e110b1a2f0d
DrRamm <kotsareu@gmail.com>
2018-07-16 09:28:56 +0300

    xenonify

--- xenonhd.mk
+++ xenonhd.mk
@@ -20,13 +20,13 @@ TARGET_SCREEN_WIDTH := 1080
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common MK stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+# Inherit some common xenonhd stuff.
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
-PRODUCT_NAME := mk_cancro
+PRODUCT_NAME := xenonhd_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
@@ -35,3 +35,6 @@ PRODUCT_MODEL := MI Cancro
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.xenonhd.maintainer=DrRamm
